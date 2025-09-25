80b901182d9cbbc79906cc2925097db49e014855
jojo2083 <jd37190+github@gmail.com>
2025-09-24 21:30:55 -0500

    added file_content function

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
 
 def main():
     working_dir = "calculator"
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    boundry_test = get_files_info(working_dir, "/bin")
-    print(boundry_test)
-    boundry_test2 = get_files_info(working_dir, "../")
-    print(boundry_test2)
-
+    print(get_file_content(working_dir, "lorem.txt"))
+    print(get_file_content(working_dir, "main.py"))
+    print(get_file_content(working_dir, "pkg/calculator.py"))
+    print(get_file_content(working_dir, "/bin/cat"))
+    print(get_file_content(working_dir, "pkg/does_not_exist.py"))
+    
 main()
 
 
